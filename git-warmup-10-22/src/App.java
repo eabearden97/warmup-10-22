51a65bd0ce4ddbb6510b6c46e879021c02537c5e
eabearden97 <ebearden@macalester.edu>
2020-10-13 10:27:52 -0500

    Created a for loop

--- git-warmup-10-22/src/App.java
+++ git-warmup-10-22/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World.");
+
+        System.out.println("Hello World. Here are numbers 1-10.");
+        for (int i=1; i<=10; i++) {
+            System.out.println(i);
+        }
+        
     }
 }
